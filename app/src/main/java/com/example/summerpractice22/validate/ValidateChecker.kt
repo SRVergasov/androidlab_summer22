052fb6f581faf17808600cb75e0628ceaece3ea9
SRVergasov <SRVergasov@stud.kpfu.ru>
2022-06-30 04:39:02 +0300

    markup corrected and add ValidateChecker.kt

---
+++ app/src/main/java/com/example/summerpractice22/validate/ValidateChecker.kt
@@ -0,0 +1,27 @@
+package com.example.summerpractice22.validate
+
+import com.example.summerpractice22.R
+
+class ValidateChecker {
+    private val EPS = 1e-7
+    private val MIN_VALUE = 1
+    private val MIN_NAME_LEN = 2
+    private val MAX_AGE_VALUE = 149
+    private val MAX_WEIGHT_VALUE = 249
+    private val MAX_HEIGHT_VALUE = MAX_WEIGHT_VALUE
+
+    private fun validName(name: String = ""): Boolean = name.length > MIN_NAME_LEN
+    private fun validAge(age: Int = 0): Boolean =
+        age in MIN_VALUE..MAX_AGE_VALUE
+    private fun validHeight(height: Int = 0): Boolean =
+        height in MIN_VALUE..MAX_HEIGHT_VALUE
+    private fun validWeight(weight: Double = 0.0): Boolean =
+        (weight - MIN_VALUE > EPS) && (weight - MAX_WEIGHT_VALUE < EPS)
+
+    fun check(
+        name: String,
+        age: Int,
+        height: Int,
+        weight: Double,
+    ): Boolean = validName(name) && validAge(age) && validHeight(height) && validWeight(weight)
+}
